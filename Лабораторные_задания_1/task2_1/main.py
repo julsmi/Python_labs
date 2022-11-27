6ae4c4d62ac1bb619e163a484600f8bfdfde801b
julsmi <115922369+julsmi@users.noreply.github.com>
2022-11-27 16:58:34 +0300

    Delete Лабораторные_задания_1 directory

--- Лабораторные_задания_1/task2_1/main.py
+++
@@ -1,7 +0,0 @@
-def task(camel_case_str: str) -> str:
-    return "".join(filter(str.islower, camel_case_str))  # TODO отфильтровать только буквы нижнего регистра
-
-
-if __name__ == "__main__":
-    word = "AbCdEfGh"
-    print(task(word))
--- Лабораторные_задания_1/task5_2/main.py
+++
@@ -1,8 +0,0 @@
-def task() -> list:
-    temp_tuple = (0, 36.6, 100)
-
-    return list(map(lambda x: (x * 9/5) + 32, temp_tuple))  # TODO  вернуть список температур по Фаренгейту
-
-
-if __name__ == "__main__":
-    print(task())
