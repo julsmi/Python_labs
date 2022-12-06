cb60671378e3161e1bb43e5ff694d3eca9eb36cc
julsmi <115922369+julsmi@users.noreply.github.com>
2022-12-06 21:32:04 +0300

    Лабораторные_работы_110_2

---
+++ task3_1.py
@@ -0,0 +1,21 @@
+def positive_check(fn):
+    def wrapper(arg):
+        # TODO написать проверку положительности аргумента arg
+        if arg < 0:
+            raise ValueError("Аргумент функции не является положительным числом")
+        result = fn(arg)
+        return result
+
+    return wrapper
+# TODO задекорировать функцию
+
+
+@positive_check
+def some_func(num: int):
+    ...
+
+
+if __name__ == "__main__":
+    some_func(5)  # всё хорошо
+
+    some_func(-5)  # должна быть вызвана ошибка ValueError
